7eebe92d7735c3c0cc836827108412dbc5d38ca3
Ryan Grubbs <ryan.grubbs.15@cnu.edu>
2019-02-24 22:17:16 -0500

    message

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	$dbname = "capstonedb";
 
 	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
+	$conn = new mysqli($servername, $username, $password);
 
 	// Check connection
 	if ($conn->connect_error) {
