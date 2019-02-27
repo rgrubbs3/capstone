e26df6e4989b327005470d395900f359c33e4bbc
Ryan Grubbs <ryan.grubbs.15@cnu.edu>
2019-02-27 00:22:22 -0500

    fighting with php

--- index.php
+++ index.php
@@ -69,15 +69,15 @@
 
 				
 
-				echo $result;
-			//if ($result->num_rows > 0) {
+				
+			if ($result->num_rows > 0) {
    				// output data of each row
-			//    while($row = $result->fetch_assoc()) {
-			//        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-			//    }
-			//} else {
-   	 		//	echo "0 results";
-			//}
+			    while($row = $result->fetch_assoc()) {
+			        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+			    }
+			} else {
+   	 			echo "0 results";
+			}
 
 			$conn->close();
 			?>
